305843aaaa1c639e5babbc8b9b088a05e45ab154
Nguyendai202 <forworkainguyendai@gmail.com>
2024-07-05 22:29:19 +0700

    remove cache

--- utils/standar_deviation.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv("test_done1.csv")
-result_df = df.groupby('Label')['Values'].std()
-print(result_df)
-result_df.to_csv("standard_deviation_orange.csv", index=False)
